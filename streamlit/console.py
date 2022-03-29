72bc8f7b5e65de6979cec8af260c94d8c5c361d4
MaciejOlech21 <96519912+MaciejOlech21@users.noreply.github.com>
2022-03-29 15:21:12 +0200

    Ready Streamlit app

--- streamlit/console.py
+++ streamlit/console.py
@@ -2,40 +2,104 @@ import streamlit as st
 import przyzyciowa as pz
 import rozplodowa as rz
 import numpy as np
-#dataFrameSerialization = "legacy"
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+@st.cache
+def convert_df(df):
+     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+     return df.to_csv().encode('utf-8')
 
-date_from = st.sidebar.date_input("Data początkowa w formacie rrrr-mm-dd")
-date_to = st.sidebar.date_input("Data końcowa w formacie rrrr-mm-dd")
+uploaded_files = st.file_uploader("Proszę wybrać pliki csv", accept_multiple_files=True)
 
-date_from = np.datetime64(date_from)
-date_to = np.datetime64(date_to)
+choose = st.selectbox("Proszę wybrać",['Rozpłodowa','Przyżyciowa'])
+if choose == 'Rozpłodowa':
+    date_from = st.sidebar.date_input("Data początkowa")
+    date_to = st.sidebar.date_input("Data końcowa")
+    date_from = np.datetime64(date_from)
+    date_to = np.datetime64(date_to)
 
-for i in uploaded_files:
-    pz.write_df_on_server(i, i.name)
+    try:
+        for i in uploaded_files:
+            pz.write_df_on_server(i, i.name)
 
-data2 = rz.concat_df()
-data3 = rz.prepare_df2(data2[0],data2[1],date_from,date_to)
-data3['Liczba sutków lochy'] = data3['Liczba sutków lochy'].fillna('-')
-data3['Wiek w dniu pierwszego oprosienia(dni)'] = data3['Wiek w dniu pierwszego oprosienia(dni)'].fillna('-')
-st.dataframe(data3)
+        data2 = rz.concat_df()
+        data3 = rz.prepare_df2(data2[0], data2[1], date_from, date_to)
+        st.subheader('Rozpłodowa Rasa')
+        st.dataframe(data3[0])
+
+        csv2 = convert_df(data3[0])
+        st.download_button(
+            label="Download data as CSV",
+            data=csv2,
+            mime='text/csv',
+            file_name='rozplodowa_rasa.csv',
+        )
+        st.subheader('Rozpłodowa rasa/region')
+        st.dataframe(data3[1])
+        csv3 = convert_df(data3[1])
+        st.download_button(
+            label="Download data as CSV",
+            data=csv3,
+            mime='text/csv',
+            file_name='rozplodowa_rasa_region.csv',
+        )
+        st.subheader('Rozpłodowa czysto rasowa i mieszańce')
+        st.dataframe(data3[2])
+        csv4 = convert_df(data3[2])
+        st.download_button(
+            label="Download data as CSV",
+            data=csv4,
+            mime='text/csv',
+            file_name='rozplodowa_krzyzowki.csv',
+        )
+        st.header('Rozpłodowa użytkowość lochy w kolejno następujących po sobie miotach')
+        st.dataframe(data3[3])
+        csv5 = convert_df(data3[3])
+        st.download_button(
+            label="Download data as CSV",
+            data=csv5,
+            mime='text/csv',
+            file_name='rozplodowa_rasa_miot.csv',
+        )
+    except:
+        st.write("Brak załadowanych plików lub wprowadzonych danych")
+
+
+
+elif choose == 'Przyżyciowa':
+    date = st.sidebar.text_input("Proszę wprowadzić rok")
+    st.sidebar.write("Proszę wprowadzić liczbę odpowiadającą płci")
+    st.sidebar.write("1 - knur ")
+    gender = st.sidebar.text_input("2 -locha")
+
+    try:
+        for i in uploaded_files:
+            pz.write_df_on_server(i, i.name)
+
+        data = pz.prepare_df(pz.concat_df(),gender,date)
+        st.subheader('Średnie wyniki oceny przyżyciowej według rasy w poszczególnych filiach "POLSUS"')
+        st.write(data[0])
+
+        csv1 = convert_df(data[0])
+        st.download_button(
+            label="Download data as CSV",
+            data=csv1,
+            mime='text/csv',
+            file_name='przyzyciowa_rasa_region.csv',
+        )
+        #pz.df_to_file(data[0], 'przyzyciowa_rasa_region.csv')
+        st.subheader('Średnie wyniki oceny przyżyciowej według rasy')
+        st.write(data[1])
+
+        csv = convert_df(data[1])
+        st.download_button(
+        label = "Download data as CSV",
+        data = csv,
+        mime = 'text/csv',
+        file_name = 'przyzyciowa_rasa.csv',
+        )
+    except:
+        st.write("Brak załadowanych plików lub wprowadzonych danych")
 
 
 
 
-
-#try:
-#    data = pz.prepare_df(cd.concat_df())
-#    st.subheader('Średnie wyniki oceny przyżyciowej według rasy w poszczególnych filiach "POLSUS"')
-#    st.write(data[0])
-#    #pz.df_to_file(data[0], 'przyzyciowa_rasa_region.csv')
-#    st.subheader('Średnie wyniki oceny przyżyciowej według rasy')
-#    st.write(data[1])
-    #pz.df_to_file(data[1], 'przyzyciowa_rasa.csv')
-#except:
-#    st.write("Brak załadowanych plików")
-#try:
-#    st.write(cd.show_df('final.txt'))
-#except:
-#    st.write("brak")
--- streamlit/rozplodowa.py
+++ streamlit/rozplodowa.py
@@ -1,9 +1,6 @@
-# importuje niezbędne biblioteki
 import pandas as pd
 import numpy as np
-import os
 import glob
-from datetime import datetime
 
 def concat_df(extension = 'csv'):
     DFL = pd.DataFrame()
@@ -17,6 +14,8 @@ def concat_df(extension = 'csv'):
         DFM = pd.concat([DFM, b])
 
     return [DFL,DFM]
+
+    # Rozpłodowa rasa
 
 def prepare_df2(DFL,DFM,date_from = 2020-12-12 ,date_to = 2021-12-11):
     DFL['NMIOT'] = 1
@@ -36,8 +35,7 @@ def prepare_df2(DFL,DFM,date_from = 2020-12-12 ,date_to = 2021-12-11):
     DFM = DFM.rename(columns={'LWOJ': 'LWOJ1'})
     # Deklaruje listę kolumn którę będą mi potrzebne
     cols2 = ['LPROS1', 'LRASA1', 'LDUR', 'LDOP', 'LRASAOJ', 'LIL11', 'LIL21', 'LWAGA1', 'M_WAGUR', 'LDBRAK', 'NMIOT',
-             'LREJ1',
-             'LWOJ1', 'LSEK1', 'LSTA1', 'LSUT1', 'NROJM1']
+             'LREJ1', 'LWOJ1', 'LSEK1', 'LSTA1', 'LSUT1', 'NROJM1']
     DFM = DFM[cols2]
 
     # Wypełniam NaN'y zerami
@@ -113,26 +111,41 @@ def prepare_df2(DFL,DFM,date_from = 2020-12-12 ,date_to = 2021-12-11):
         newDT = DT[DT[col] == number]
         return newDT
 
+
     # Funkcja sortuje dane po rasie matki i rasie ojca
     def sorted2DT(DT, number1, number2):
         newDT = DT[(DT.LRASA1 == number1) & (DT.LRASAOJ == number2)]
         return newDT
 
-    def months(DT, col, col2):
-        newDT = DT[DT[col] != DT[col2]]
+    def sorted3DT(DT, number1, number2):
+        newDT = DT[(DT.LRASA1 == number1) & (DT.NMIOT == number2)]
         return newDT
 
+
     # Funkcja wylicza wszystkie niezbędne wartości i przypisuje je do DF
-    def all_count(DF3, new):
-        new['liczba loch'] = len(DF3.drop_duplicates(subset='LPROS1', keep="first").copy())
-        new['liczba ocenionych miotów ogółem'] = len(DF3)
-        new['liczba ocenionych miotów pierw.'] = len(DF3[DF3['NMIOT'] == 1])
-        new['Liczba prosiąt urodzonych w miocie'] = np.around(DF3['LIL11'].mean(), decimals=2)
-        new['Liczba prosiąt w 21 dniu życia'] = np.around(DF3['LIL21'].mean(), decimals=2)
-        new['Straty prosiąt do 21 dnia życia(%)'] = np.around(DF3['ST21'].mean(), decimals=2)
-        new['Wiek w dniu pierwszego oprosienia(dni)'] = np.around(DF3['POP'].mean(), decimals=2)
-        new['Okres miedzymiotu(dni)'] = np.around(DF3['OMM'].mean(), decimals=2)
-        new['Liczba sutków lochy'] = np.around(DF3['ISUT'].mean(), decimals=2)
+
+    def all_count(DT, new):
+        new['liczba loch'] = len(DT.drop_duplicates(subset='LPROS1', keep="first").copy())
+        new['liczba ocenionych miotów ogółem'] = len(DT)
+        new['liczba ocenionych miotów pierw.'] = len(DT[DT['NMIOT'] == 1])
+        new['Liczba prosiąt urodzonych w miocie'] = np.around(DT['LIL11'].mean(), decimals=2)
+        new['Liczba prosiąt w 21 dniu życia'] = np.around(DT['LIL21'].mean(), decimals=2)
+        new['Straty prosiąt do 21 dnia życia(%)'] = np.around(DT['ST21'].mean(), decimals=2)
+        new['Wiek w dniu pierwszego oprosienia(dni)'] = np.around(DT['POP'].mean(), decimals=2)
+        new['Okres miedzymiotu(dni)'] = np.around(DT['OMM'].mean(), decimals=2)
+        new['Liczba sutków lochy'] = np.around(DT['ISUT'].mean(), decimals=2)
+
+        return new
+
+    def all_count2(DT, new, rasa):
+        miot = DT['NMIOT'].unique()
+        new['Kolejny miot'] = miot[0]
+        new['Mioty szt.'] = len(DT)
+        new['Mioty %'] = np.around(len(DT) / len(DF3[(DF3.LRASA1 == rasa)]) * 100, decimals=2)
+        new['Liczba prosiąt urodzonych w miocie'] = np.around(DT['LIL11'].mean(), decimals=2)
+        new['Liczba prosiąt w 21 dniu życia'] = np.around(DT['LIL21'].mean(), decimals=2)
+        new['Straty prosiąt do 21 dnia życia(%)'] = np.around(DT['ST21'].mean(), decimals=2)
+        new['Okres międzymiotu(dni)'] = np.around(DT['OMM'].mean())
 
         return new
 
@@ -175,9 +188,159 @@ def prepare_df2(DFL,DFM,date_from = 2020-12-12 ,date_to = 2021-12-11):
     # Łącze wyniki ras w jeden DF i ustalam Rasę jako index
     rasaDF = pd.concat([WBP, PBZ, HAMP, DUR, PIET, L99, PULA, ZB, ZPS])
     rasaDF.set_index('Rasa', inplace=True)
-    #rasaDF['Liczba sutków lochy'] = rasaDF['Liczba sutków lochy'].fillna('-')
-    #rasaDF['Wiek w dniu pierwszego oprosienia(dni)'] = rasaDF['Wiek w dniu pierwszego oprosienia(dni)'].fillna('-')
+    rasaDF['Liczba sutków lochy'] = rasaDF['Liczba sutków lochy'].astype(str)
+    rasaDF['Wiek w dniu pierwszego oprosienia(dni)'] = rasaDF['Wiek w dniu pierwszego oprosienia(dni)'].astype(str)
+    rasaDF['Liczba sutków lochy'].replace('nan','-',inplace=True)
+    rasaDF['Wiek w dniu pierwszego oprosienia(dni)'].replace('nan','-',inplace=True)
 
-    return rasaDF
+
+    # Rozplodowa rasa/region
+
+    # Deklaruje słoknik miast
+    dic_miasta = {1: 'Poznań',
+                  2: 'Warszawa',
+                  3: 'Poznań',
+                  4: 'Białystok',
+                  6: 'Bydgoszcz',
+                  8: 'Gdańsk',
+                  10: 'Katowice',
+                  12: 'Kielce',
+                  14: 'Koszalin',
+                  16: 'Kraków',
+                  18: 'Lublin',
+                  20: 'Łódź',
+                  22: 'Olsztyn',
+                  24: 'Opole',
+                  26: 'Poznań',
+                  28: 'Rzeszów',
+                  32: 'Wrocław',
+                  34: 'Zielona Góra',
+                  98: '98'}
+
+    # Deklaruje listę wydzielonych ras
+    race_list = [wbp, pbz, hamp, dur, piet, l99, zb, zps, pula]
+
+    def create_cityDT(city, race):
+        temp = pd.DataFrame(columns=['Rasa', 'Miasto', 'liczba loch', 'liczba ocenionych miotów ogółem',
+                                     'liczba ocenionych miotów pierw.', 'Liczba prosiąt urodzonych w miocie',
+                                     'Liczba prosiąt w 21 dniu życia', 'Straty prosiąt do 21 dnia życia(%)',
+                                     'Liczba sutków lochy',
+                                     'Wiek w dniu pierwszego oprosienia(dni)', 'Okres miedzymiotu(dni)'], index=[1])
+        temp['Miasto'] = city
+        if race is wbp:
+            temp['Rasa'] = "WBP"
+        elif race is pbz:
+            temp['Rasa'] = "PBZ"
+        elif race is hamp:
+            temp['Rasa'] = "Hampshire"
+        elif race is dur:
+            temp['Rasa'] = "Duroc"
+        elif race is piet:
+            temp['Rasa'] = "Pietrain"
+        elif race is l99:
+            temp['Rasa'] = "Linia 99"
+        elif race is zb:
+            temp['Rasa'] = "Złotnicka Biała"
+        elif race is zps:
+            temp['Rasa'] = "Złotnicka Pstra"
+        elif race is pula:
+            temp['Rasa'] = "Puławska"
+        return temp
+
+    def createDT2(name):
+        temp = pd.DataFrame(columns=['Kolejny miot', 'Rasa', 'Mioty szt.',
+                                     'Mioty %', 'Liczba prosiąt urodzonych w miocie',
+                                     'Liczba prosiąt w 21 dniu życia', 'Straty prosiąt do 21 dnia życia(%)',
+                                     'Okres międzymiotu(dni)'], index=[1])
+        temp['Rasa'] = name
+        return temp
+
+    CompleteCityDF = pd.DataFrame()
+
+    for city in dic_miasta:
+        for race in race_list:
+            tempDF = create_cityDT(dic_miasta[city], race)
+            tempDF2 = sortedDT(race, "LREJ1", city)
+            rdy = all_count(tempDF2, tempDF)
+            CompleteCityDF = pd.concat([CompleteCityDF, rdy])
+
+    CompleteCityDF['Okres miedzymiotu(dni)'] = CompleteCityDF['Okres miedzymiotu(dni)'].astype(str)
+    CompleteCityDF['Liczba sutków lochy'] = CompleteCityDF['Liczba sutków lochy'].astype(str)
+    CompleteCityDF['Wiek w dniu pierwszego oprosienia(dni)'] = CompleteCityDF['Wiek w dniu pierwszego oprosienia(dni)'].astype(str)
+
+    CompleteCityDF['Okres miedzymiotu(dni)'].replace('nan','-',inplace=True)
+    CompleteCityDF['Liczba sutków lochy'].replace('nan','-',inplace=True)
+    CompleteCityDF['Wiek w dniu pierwszego oprosienia(dni)'].replace('nan','-',inplace=True)
+    CompleteCityDF = CompleteCityDF.set_index(['Rasa', 'Miasto']).sort_index(ascending=True).dropna()
+
+
+    # Rozplodowa mieszańce
+
+    WBPxWBP = createDT('WBP x WBP')
+    wbpXwbp = sorted2DT(DF3, 10, 10)
+    all_count(wbpXwbp, WBPxWBP)
+
+    WBPxPBZ = createDT('WBP x PBZ')
+    wbpXpbz = sorted2DT(DF3, 10, 20).copy()
+    all_count(wbpXpbz, WBPxPBZ)
+
+    PBZxPBZ = createDT('PBZ x PBZ')
+    pbzXpbz = sorted2DT(DF3, 20, 20).copy()
+    all_count(pbzXpbz, PBZxPBZ)
+
+    PBZxWBP = createDT('PBZ x WBP')
+    pbzXwbp = sorted2DT(DF3, 20, 10).copy()
+    all_count(pbzXwbp, PBZxWBP)
+
+    DUROCxDUROC = createDT('Duroc x Duroc')
+    durocXduroc = sorted2DT(DF3, 70, 70).copy()
+    all_count(durocXduroc, DUROCxDUROC)
+
+    DUROCxPIETRAIN = createDT('Duroc x Pietrain')
+    durocXpietrain = sorted2DT(DF3, 70, 80).copy()
+    all_count(durocXpietrain, DUROCxPIETRAIN)
+
+    PIETRAINxPIETRAIN = createDT('Pietrain x Pietrain')
+    pietrainXpietrain = sorted2DT(DF3, 80, 80).copy()
+    all_count(pietrainXpietrain, PIETRAINxPIETRAIN)
+
+    PIETRAINxDUROC = createDT('Pietrain X Duroc')
+    pietrainXduroc = sorted2DT(DF3, 80, 70).copy()
+    all_count(pietrainXduroc, PIETRAINxDUROC)
+
+    krzyzowkiDF = pd.concat([WBPxWBP, WBPxPBZ, PBZxPBZ, PBZxWBP, DUROCxDUROC,
+                             DUROCxPIETRAIN, PIETRAINxPIETRAIN, PIETRAINxDUROC])
+    krzyzowkiDF.set_index('Rasa', inplace=True)
+
+
+    # Rozpłodowa rasa/miot
+
+    race_dict = {
+        10: "WBP",
+        20: "PBZ",
+        30: "Złotnicka Biała",
+        40: "Póławska",
+        50: "Złotnicka Pstra",
+        60: "Hampshire",
+        70: "Duroc",
+        80: "Pietrain",
+        90: "Linia 990"}
+
+
+    completeDF = pd.DataFrame()
+    for race in race_dict:
+        dokladna_rasa = DF3[(DF3.LRASA1 == race)]
+        mioty = dokladna_rasa['NMIOT'].unique()
+        for i in mioty:
+            aDF = createDT2(race_dict[race])
+            bDF = sorted3DT(DF3, race, i)
+            rdy = all_count2(bDF, aDF, race)
+            completeDF = pd.concat([completeDF, rdy])
+
+    completeDF.set_index(['Rasa', 'Kolejny miot']).sort_values(by=['Rasa', 'Kolejny miot'])
+    completeDF['Okres międzymiotu(dni)'] = completeDF['Okres międzymiotu(dni)'].astype(str)
+    completeDF['Okres międzymiotu(dni)'].replace('nan','-',inplace=True)
+
+    return [rasaDF,CompleteCityDF,krzyzowkiDF,completeDF]
 
 
